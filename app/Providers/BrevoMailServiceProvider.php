723ff55d4cf0c112f87b10040ce296d6bb286e54
dharanikumar <dharaniraja2004@gmail.com>
2026-06-24 10:17:09 +0530

    Brevo service provider added

---
+++ app/Providers/BrevoMailServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Mail\MailManager;
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
+
+class BrevoMailServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        app(MailManager::class)->extend('brevo', function (array $config) {
+            $factory = new BrevoTransportFactory();
+
+            return $factory->create(new Dsn(
+                'brevo+api',
+                'default',
+                $config['key'],
+            ));
+        });
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\BrevoMailServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
 ];
