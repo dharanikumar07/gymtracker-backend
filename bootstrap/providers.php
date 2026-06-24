9b8acdb5f1ac535cb2f7ba7cbf1f0e08fe297773
dharanikumar <dharaniraja2004@gmail.com>
2026-06-24 11:46:21 +0530

    bugsnag registered as providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
     App\Providers\BrevoMailServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
 ];
